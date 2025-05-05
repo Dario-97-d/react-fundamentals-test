d30af20dddf6825e5c2f3d72e16faa8784406e45
Dario Dias <dariodias97@gmail.com>
2025-05-05 13:00:27 +0100

    Add TasksPage for general listing of Tasks

---
+++ src/constants.js
@@ -0,0 +1,7 @@
+export const API_URL = 'http://localhost:3001';
+
+export const priorityClasses = {
+  baixa: 'priorityLow',
+  media: 'priorityMedium',
+  alta: 'priorityHigh',
+}
--- src/pages/TasksPage.jsx
+++ src/pages/TasksPage.jsx
@@ -1,4 +1,57 @@
+import { useEffect, useState } from 'react'
+import { NavLink } from 'react-router-dom'
+import { API_URL, priorityClasses } from '../constants'
+import axios from 'axios'
+import styles from './TasksPage.module.css'
+
 export default function TasksPage()
 {
-  return <p>All Tasks</p>
+  const [message, setMessage] = useState('')
+  const [tasks, setTasks] = useState([])
+
+  // Load Tasks.
+  useEffect(() => {
+    setMessage('')
+    axios
+      .get(`${API_URL}/tasks`)
+      .then(response => setTasks(response.data))
+      .catch(() => setMessage('Não foi possível carregar as tarefas.'))
+  }, [])
+
+  return (<>
+  
+    <h1>Todas as Tarefas</h1>
+
+    <div className="main-container">
+      {message && <p>{message}</p>}
+
+      {tasks.length > 0 &&
+        <div className={styles.grid}>
+          <div className={styles.gridRowHeader}>
+              <div>Título</div>
+              <div>Prioridade</div>
+              <div>Feita</div>
+          </div>
+
+          {tasks.map(t => (
+            <NavLink key={t.id} to={`/tasks/${t.id}`} className={styles.gridRow}>
+              
+              <div className={styles.title}>
+                {t.title}
+              </div>
+              
+              <div className={priorityClasses[t.priority]}>
+                {t.priority}
+              </div>
+
+              <div>{t.done ? '✅' : '⭕'}</div>
+
+            </NavLink>
+              
+          ))}
+        </div>
+      }
+    </div>
+  
+  </>)
 }
