e8a8c14578a2db70d7b16e33b1cd59e26538cb2c
Dario-97-d <dariodias97@gmail.com>
2026-03-03 15:54:24 +0000

    Prevent 404
    - Added _redirects file to prevent 404 on refresh.
    - Added generic React Route.

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Navigate to={"/tasks"} replace/>}></Route>
+        <Route path="*" element={<Navigate to={"/tasks"} replace/>}></Route>
         
         <Route element={<Layout />}>
           <Route path="/tasks" element={<TasksPage />}></Route>
